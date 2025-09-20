472d1ee64ed5e8bd88315ff697d32b7f35c1252c
Aishix7 <hithaishi714@gmail.com>
2025-09-20 22:47:03 +0530

    added two buttons 'ingredients' and 'How-to' for each card and modified the styling

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -4,17 +4,25 @@ export const RecipeCard = ({ title, image }) => {
   const FastImage = image + "/medium";
   return (
     <div className="flex flex-row p-8 m-auto">
-      <div className="bg-white shadow-lg rounded-2xl overflow-hidden w-64 hover:scale-105 transition-transform duration-300">
+      <div className="bg-white shadow-lg rounded-2xl border border-black overflow-hidden w-75 hover:scale-105 transition-transform duration-300">
         <img
           src={FastImage}
           //   src={image}
           alt={title}
-          className="w-full h-48 flex items-center justify-center object-cover"
+          className="w-full h-48 flex border border-black items-center justify-center object-cover"
         />
         <div className="p-4">
-          <h2 className=" flex items-center justify-center text-lg font-semibold text-gray-800">
+          <h2 className=" flex items-center justify-center text-lg font-bold text-gray-800">
             {title}
           </h2>
+          <div className="flex items-center justify-center space-x-4 p-4">
+            <button className="p-2 font-semibold border border-black shadow-lg rounded-xl bg-orange-400 w-full">
+              Ingredients
+            </button>
+            <button className="p-2 font-semibold border border-black rounded-xl shadow-lg bg-orange-400 w-full">
+              How-to
+            </button>
+          </div>
         </div>
       </div>
     </div>
