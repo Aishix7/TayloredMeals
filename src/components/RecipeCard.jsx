6cfcc4d5de536f129f823cc0193e7c43f5390d39
Aishix7 <hithaishi714@gmail.com>
2025-09-20 22:32:19 +0530

    update image + '/medium' for faster loading of image

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 export const RecipeCard = ({ title, image }) => {
+  const FastImage = image + "/medium";
   return (
     <div className="flex flex-row p-8 m-auto">
       <div className="bg-white shadow-lg rounded-2xl overflow-hidden w-64 hover:scale-105 transition-transform duration-300">
         <img
-          src={image}
+          src={FastImage}
+          //   src={image}
           alt={title}
           className="w-full h-48 flex items-center justify-center object-cover"
         />
