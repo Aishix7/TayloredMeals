0407c37f1f0c1dd096263a3f7995cbe6fa2f0062
Aishix7 <hithaishi714@gmail.com>
2025-09-20 15:33:45 +0530

    added onSearch function for searchBar in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import "./App.css";
 import { SearchBar } from "./components/SearchBar";
 
 export default function App() {
+  const handleSearch = (ingredient) => {
+    console.log("you entered", ingredient);
+  };
   return (
     <div>
-      <SearchBar />
+      <SearchBar onSearch={handleSearch} />
     </div>
   );
 }
