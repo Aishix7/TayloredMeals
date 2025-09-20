759c49a546f1e7f9526658b7ea2b4b5222ecc442
Aishix7 <hithaishi714@gmail.com>
2025-09-20 20:42:40 +0530

    added state for handling api response and 'recipes' as props from recipeList.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
 import "./App.css";
+import { useState } from "react";
 import { SearchBar } from "./components/SearchBar";
 import { RecipeList } from "./pages/RecipeList";
+import { getMeal } from "./api/PostApi";
 
 export default function App() {
-  const handleSearch = (ingredient) => {
-    console.log("you entered", ingredient);
+  const [recipes, setRecipes] = useState([]);
+
+  const handleSearch = async (ingredient) => {
+    console.log("You entered:", ingredient);
+    try {
+      const response = await getMeal(ingredient);
+      console.log("API Response:", response.data);
+
+      if (response.data.meals) {
+        setRecipes(response.data.meals);
+      } else {
+        alert("Sorry Taylor! No meal with your specified ingredient");
+        setRecipes([]);
+      }
+    } catch (error) {
+      console.error("Error fetching recipes:", error);
+      setRecipes([]);
+    }
   };
+
   return (
     <div>
       <SearchBar onSearch={handleSearch} />
-      <RecipeList />
+      <RecipeList recipes={recipes} />
     </div>
   );
 }
