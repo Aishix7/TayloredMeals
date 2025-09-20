fcfdf574160a4e5247d56a6911a1b078d9680370
Aishix7 <hithaishi714@gmail.com>
2025-09-20 23:12:43 +0530

    fetch meal details on ingredients button click and show IngredientsList popup

--- src/pages/RecipeList.jsx
+++ src/pages/RecipeList.jsx
@@ -1,18 +1,30 @@
 import React, { useState } from "react";
 import { RecipeCard } from "../components/RecipeCard";
 import { IngredientsList } from "../components/IngredientsList";
+import { getMealDetails } from "../api/PostApi";
 
-export const RecipeList = (props) => {
+export const RecipeList = ({ recipes }) => {
   const [selectedRecipe, setSelectedRecipe] = useState(null);
+
+  const handleShowIngredients = async (idMeal) => {
+    try {
+      const response = await getMealDetails(idMeal);
+      if (response.data.meals) {
+        setSelectedRecipe(response.data.meals[0]);
+      }
+    } catch (error) {
+      console.error("Error fetching meal details:", error);
+    }
+  };
 
   return (
     <div className="flex flex-wrap gap-6 justify-center p-8">
-      {props.recipes?.map((recipe) => (
+      {recipes?.map((recipe) => (
         <RecipeCard
           key={recipe.idMeal}
           title={recipe.strMeal}
           image={recipe.strMealThumb}
-          onShowIngredients={() => setSelectedRecipe(recipe)}
+          onShowIngredients={() => handleShowIngredients(recipe.idMeal)}
         />
       ))}
 
