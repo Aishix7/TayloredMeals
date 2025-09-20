ac141f4b87200bb06152e5ea82393cfa4a9d329a
Aishix7 <hithaishi714@gmail.com>
2025-09-20 22:30:47 +0530

    added props to handle array of recipes

--- src/pages/RecipeList.jsx
+++ src/pages/RecipeList.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { RecipeCard } from "../components/RecipeCard";
 
-export const RecipeList = ({ recipes }) => {
+export const RecipeList = (props) => {
   return (
     <div className="flex flex-wrap gap-6 justify-center p-8">
-      {recipes?.map((recipe) => (
+      {props.recipes?.map((recipe) => (
         <RecipeCard
           key={recipe.idMeal}
           title={recipe.strMeal}
