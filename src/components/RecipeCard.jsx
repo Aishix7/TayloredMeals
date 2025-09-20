b51e23845200b3577ae10b813464604aae47054b
Aishix7 <hithaishi714@gmail.com>
2025-09-20 20:44:21 +0530

    fetching title and image from api into cards

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
-export const RecipeCard = () => {
+export const RecipeCard = ({ title, image }) => {
   return (
     <div className="flex flex-row p-8 m-auto">
       <div className="bg-white shadow-lg rounded-2xl overflow-hidden w-64 hover:scale-105 transition-transform duration-300">
         <img
-          src=""
-          alt="Photo"
+          src={image}
+          alt={title}
           className="w-full h-48 flex items-center justify-center object-cover"
         />
         <div className="p-4">
           <h2 className=" flex items-center justify-center text-lg font-semibold text-gray-800">
-            Title
+            {title}
           </h2>
         </div>
       </div>
