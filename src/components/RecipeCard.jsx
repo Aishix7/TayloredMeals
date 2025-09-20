e44a5a59e6239c8829e2b7e512a20a40d9c0bafb
Aishix7 <hithaishi714@gmail.com>
2025-09-20 20:00:36 +0530

    added styling for Meal Card

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,5 +1,20 @@
 import React from "react";
 
 export const RecipeCard = () => {
-  return <div>RecipeCard</div>;
+  return (
+    <div className="flex flex-row m-auto">
+      <div className="bg-white shadow-lg rounded-2xl overflow-hidden w-64 hover:scale-105 transition-transform duration-300">
+        <img
+          src=""
+          alt="Photo"
+          className="w-full h-48 flex items-center justify-center object-cover"
+        />
+        <div className="p-4">
+          <h2 className=" flex items-center justify-center text-lg font-semibold text-gray-800">
+            Title
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
 };
