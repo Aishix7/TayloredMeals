83cc14d2c841ce3311ebb8e405b468d0c64773a5
Aishix7 <hithaishi714@gmail.com>
2025-09-20 15:32:35 +0530

    Search Bar can now handle input

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { SearchIcon } from "lucide-react";
 import { getMeal } from "../api/PostApi";
-export const SearchBar = () => {
+export const SearchBar = ({ onSearch }) => {
+  const [ingredient, setIngredient] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (ingredient.trim() !== "") {
+      onSearch(ingredient);
+    } else {
+      alert("Enter some ingredient Taylor!");
+    }
+  };
   return (
     <>
       <div>
-        <div className="p-10 m-auto flex items-center justify-center space-x-4">
+        <form
+          onSubmit={handleSubmit}
+          className="p-10 m-auto flex items-center justify-center space-x-4"
+        >
           <input
             className="p-4 border border-[#939393] rounded-lg"
             type="text"
             placeholder="Enter an ingredient"
+            value={ingredient}
+            onChange={(e) => setIngredient(e.target.value)}
           />
           <button
             type="submit"
@@ -17,7 +32,7 @@ export const SearchBar = () => {
           >
             <SearchIcon className="text-white w-10 h-10 bg-black" />
           </button>
-        </div>
+        </form>
       </div>
     </>
   );
