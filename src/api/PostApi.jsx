8bec5607d731389d3faaeae55524e1223ae79503
Aishix7 <hithaishi714@gmail.com>
2025-09-20 13:44:29 +0530

    added MealDBApi

--- src/api/PostApi.jsx
+++ src/api/PostApi.jsx
@@ -1,1 +1,9 @@
 import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://www.themealdb.com/api/json/v1/1/",
+});
+
+export const getMeal = (ingredient) => {
+  return api.get(`filter.php?i=${ingredient}`);
+};
